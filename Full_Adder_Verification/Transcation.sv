016d4d941d25c73c084b4ec953e690435f54817c
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-12-07 12:28:26 +0530

    Update Transcation.sv

--- Full_Adder_Verification/Transcation.sv
+++ Full_Adder_Verification/Transcation.sv
@@ -1,1 +1,12 @@
-01
+class transaction;
+  rand bit a;
+  rand bit b;
+  rand bit c;
+  bit sum;
+  bit carry; 
+  
+  function void display(string s);
+    $display("%s",s);
+    $display($time,"a=%0d b=%0d c=%0d | sum=%0d carry=%0d",a,b,c,sum,carry);
+  endfunction
+endclass
