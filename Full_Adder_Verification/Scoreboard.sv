886efa07665ea1a9eb5911c53f07005195b41319
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-12-07 12:31:20 +0530

    Update Scoreboard.sv

--- Full_Adder_Verification/Scoreboard.sv
+++ Full_Adder_Verification/Scoreboard.sv
@@ -1,1 +1,21 @@
-04
+class scoreboard;
+  mailbox m2s;
+  
+  function new(mailbox m2s);
+    this.m2s = m2s;
+  endfunction
+  
+  task score();
+    transaction tr;
+    repeat(2) begin
+      m2s.get(tr);
+        tr.display("fr_scr_b");
+      if ((tr.a^tr.b^tr.c)==tr.sum && ((tr.a & tr.b )|(tr.b & tr.c)|(tr.c & tr.a)) == tr.carry)
+      $display("Result:pass");
+      else
+        $display("Result:fail");
+    end
+  endtask
+endclass
+     
+  
