617eed1a5a9540c12dcdc7801059ef2e6d07f838
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-12-07 12:29:00 +0530

    Update Generator.sv

--- Full_Adder_Verification/Generator.sv
+++ Full_Adder_Verification/Generator.sv
@@ -1,1 +1,18 @@
-02
+class generator;
+  transaction tr;
+  mailbox g2d;
+  
+  function new(mailbox g2d);
+    this.g2d = g2d;
+  endfunction
+  
+  task gene();
+    repeat (4) begin
+      tr = new();
+      tr.randomize();
+      $display("generator is ready");
+      g2d.put(tr);
+      #3;
+    end
+  endtask
+endclass
