eb28b614beea9d8ac443cebc2214a117fd4a4889
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-12-07 12:33:18 +0530

    Update Testbench.sv

--- Full_Adder_Verification/Testbench.sv
+++ Full_Adder_Verification/Testbench.sv
@@ -1,1 +1,9 @@
-08
+// or browse Examples
+`include "Interface.sv"
+`include "Test.sv"
+module testbench;
+  inter vintf();
+  test tst(vintf);
+  full_adder(.a(vintf.a),.b(vintf.b),.c(vintf.c),.sum(vintf.sum),.carry(vintf.carry));
+endmodule
+
