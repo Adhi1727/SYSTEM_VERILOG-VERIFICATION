d82c886e1f4ef5c5620df0c263127cb85a7a740e
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-12-11 21:39:02 +0530

    Update Transaction.sv

--- Half_Adder_Verification/Transaction.sv
+++ Half_Adder_Verification/Transaction.sv
@@ -1,1 +1,11 @@
-..
+class transaction;
+  rand bit a;
+  rand bit b;
+  bit sum;
+  bit carry;
+  
+  function void display(string s);
+    $display("%s",s);
+    $display($time,"a = %0d, b = %0d | sum = %0d, carry = %0d",a,b,sum,carry);
+  endfunction
+endclass
