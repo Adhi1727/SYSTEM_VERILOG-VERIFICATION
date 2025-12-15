450e7fc45ad2b2fc96f93f73471a7181754226dd
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-12-15 19:15:20 +0530

    Update Interface.sv

--- Half_Adder_Verification/Interface.sv
+++ Half_Adder_Verification/Interface.sv
@@ -1,9 +1,6 @@
-`include "environment.sv"
-program test(inter vir);
-  environment env;
-  
-  initial begin
-    env = new(vir);
-    env.run();
-  end
-endprogram
+interface inter;
+  logic a;
+  logic b;
+  logic sum;
+  logic carry;
+endinterface
