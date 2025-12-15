df72fbcc9215ead21ff0dcb6f763355a156abd59
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-12-15 11:51:20 +0530

    Update Driver.sv

--- Half_Adder_Verification/Driver.sv
+++ Half_Adder_Verification/Driver.sv
@@ -1,1 +1,21 @@
-//
+class driver;
+  transaction tr;
+  mailbox g2d;
+  virtual inter vir;
+  
+  function new(virtual inter vir, mailbox g2d);
+    this.vir = vir;
+    this.g2d = g2d;
+  endfunction
+  
+  task dir();
+    repeat (2) begin
+      tr = new();
+      g2d.get(tr);
+      vir.a <= tr.a;
+      vir.b <= tr.b;
+      tr.display("driver class signals");
+      #2;
+    end
+  endtask
+endclass
