042d81e64d10b7b5871648490349ae6aac8b2983
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-12-15 19:19:59 +0530

    Create Output.sv

---
+++ Half_Adder_Verification/Output.sv
@@ -0,0 +1,25 @@
+Contains Synopsys proprietary information.
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Dec 15 08:47 2025
+generator is ready
+driver class signals
+                   0a = 0, b = 1 | sum = 0, carry = 0
+monitor class signals
+                   0a = 0, b = 0 | sum = 0, carry = 0
+fsr_scr_b
+                   0a = 0, b = 0 | sum = 0, carry = 0
+Result : pass
+monitor class signals
+                   1a = 0, b = 1 | sum = 1, carry = 0
+fsr_scr_b
+                   1a = 0, b = 1 | sum = 1, carry = 0
+Result : pass
+generator is ready
+driver class signals
+                   3a = 1, b = 0 | sum = 0, carry = 0
+generator is ready
+generator is ready
+$finish at simulation time                   12
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 12 ns
+CPU Time:      0.410 seconds;       Data structure size:   0.0Mb
+Mon Dec 15 08:47:50 2025
