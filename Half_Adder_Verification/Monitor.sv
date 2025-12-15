4fbf91b42c9825f4e5f6fdf065b3bd7c134ade14
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-12-15 11:52:58 +0530

    Update Monitor.sv

--- Half_Adder_Verification/Monitor.sv
+++ Half_Adder_Verification/Monitor.sv
@@ -1,1 +1,23 @@
-//
+class monitor;
+  transaction tr;
+  mailbox m2s;
+  virtual inter vir;
+  
+  function new(virtual inter vir, mailbox m2s);
+    this.m2s = m2s;
+    this.vir = vir;
+  endfunction
+  
+  task mons();
+    repeat (2) begin
+      tr = new();
+      tr.a = vir.a;
+      tr.b = vir.b;
+      tr.sum = vir.sum;
+      tr.carry = vir.carry;
+      m2s.put(tr);
+      tr.display("monitor class signals");
+      #1;
+    end
+  endtask
+endclass
