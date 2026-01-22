bc3756fcd77213f6f46f6dbe1606b9ec051f321d
ADHIKESAVAN . M <adhik8108@gmail.com>
2026-01-22 21:13:00 +0530

    Update 06.Monitor.sv

--- D_flip_flop_verification/06.Monitor.sv
+++ D_flip_flop_verification/06.Monitor.sv
@@ -1,1 +1,22 @@
-
+class monitor;
+  transaction trans;
+  mailbox m2s;
+  virtual inter inf;
+  
+  function new(mailbox m2s, virtual inter inf);
+    this.m2s = m2s;
+    this.inf = inf;
+  endfunction
+  
+  task moni();
+    trans = new();
+    repeat(5) begin
+      @(posedge inf.clk);
+      trans.rst = inf.rst;
+      trans.d = inf.d;
+      trans.q = inf.q;
+      m2s.put(trans);
+      trans.display("MONITOR CHECKED");
+    end
+  endtask
+endclass
