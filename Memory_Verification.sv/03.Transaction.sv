dc287ea83d175ce1ec07532d6cfeee8febfbc568
ADHIKESAVAN . M <adhik8108@gmail.com>
2026-02-10 14:00:29 +0530

    Update 03.Transaction.sv

--- Memory_Verification.sv/03.Transaction.sv
+++ Memory_Verification.sv/03.Transaction.sv
@@ -5,9 +5,14 @@ class transaction;
   rand bit [7:0]wr_d;
   bit [7:0]rd_d;
   
+  constraint cons1 {addr>0; addr<10;}
+  constraint cons2 { r_w dist {1:=50,0:=50};}
+  constraint cons4 {addr!=0;wr_d!=0;}
+  
   function void display(string flag);
     $display("%s",flag);
     $display("Transaction is Ready");
   endfunction
 endclass
+  
   
