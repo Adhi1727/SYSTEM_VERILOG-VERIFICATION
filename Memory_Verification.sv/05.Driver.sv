fe3c6132f65b7ea1a556c1323b92436141975345
ADHIKESAVAN . M <adhik8108@gmail.com>
2026-02-10 14:02:33 +0530

    Update 05.Driver.sv

--- Memory_Verification.sv/05.Driver.sv
+++ Memory_Verification.sv/05.Driver.sv
@@ -9,13 +9,13 @@ class driver;
   endfunction
   
   task drv();
-    trans = new();
     repeat (10) begin
       g2d.get(trans);
-      @(negedge vinf.clk)
-      vinf.r_w <= trans.r_w;
-      vinf.addr <= trans.addr;
-      vinf.wr_d <= trans.wr_d;
+
+      @(posedge vinf.clk)
+      vinf.r_w = trans.r_w;
+      vinf.addr = trans.addr;
+      vinf.wr_d = trans.wr_d;
       trans.display("Driver is Here");
     end
   endtask
